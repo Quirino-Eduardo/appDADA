5ee6ee44c17b08033acbf111046f85a8d27634a5
Eduardo-Quirino <dev.quirino1218@gmail.com>
2024-06-08 00:52:58 -0300

    add função pegar tel.

--- script.js
+++ script.js
@@ -4,14 +4,20 @@ const carouselItems = document.querySelectorAll('.carousel-item');
 let currentIndex = 0;
 
 document.querySelector('.carousel-prev').addEventListener('click', () => {
-    currentIndex--;
-    carouselInner.style.transform = `translateX(${currentIndex * -100}%)`;
+    currentIndex = (currentIndex > 0) ? currentIndex - 1 : carouselItems.length - 1;
+    updateCarousel();
 });
 
 document.querySelector('.carousel-next').addEventListener('click', () => {
-    currentIndex++;
-    carouselInner.style.transform = `translateX(${currentIndex * -100}%)`;
+    currentIndex = (currentIndex < carouselItems.length - 1) ? currentIndex + 1 : 0;
+    updateCarousel();
 });
+
+function updateCarousel() {
+    carouselItems.forEach((item, index) => {
+        item.classList.toggle('active', index === currentIndex);
+    });
+}
 
 // Integração de formulário para pedidos via WhatsApp
 const orderForm = document.getElementById('order-form');
@@ -23,6 +29,6 @@ sendOrderButton.addEventListener('click', (e) => {
     const phone = document.getElementById('phone').value;
     const products = document.getElementById('products').value;
 
-    const whatsappUrl = `https://api.whatsapp.com/send?phone=55${phone}&text=Olá!%20Eu%20gostaria%20de%20fazer%20um%20pedido:%20${products}%20-%20${name}`;
+    const whatsappUrl = `https://api.whatsapp.com/send?phone=+5511962507584&text=Olá!%20Eu%20gostaria%20de%20fazer%20um%20pedido:%20${products}%20-%20${name}%20-%20Telefone:%20${phone}`;
     window.open(whatsappUrl, '_blank');
 });
